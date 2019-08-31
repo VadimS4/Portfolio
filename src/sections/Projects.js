8ed337a31423953514b67e3773a0b859dc44fca0
VadimS4 <github email address>
2019-08-31 16:03:50 -0700

    completed navbar

---
+++ src/sections/Projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return(
+            <div className="projects-section" id="projects"></div>
+        )
+    }
+}
+
+export default Projects;
