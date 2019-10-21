7bd24282ac364d7cbb03cac4132d2c882367066e
VadimS4 <github email address>
2019-10-20 18:07:00 -0700

    Updated most of the progress on the website

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/components/icon.min.css';
+import 'semantic-ui-css/components/card.min.css';
+import 'semantic-ui-css/components/image.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-// import IconRuby from 'react-devicon/ruby/plain-wordmark';
-// import IconRails from 'react-devicon/rails/plain-wordmark';
-// import IconNodejs from 'react-devicon/nodejs/original-wordmark';
-// import IconJavascript from 'react-devicon/javascript/original';
-// import IconReact from 'react-devicon/react/original-wordmark';
-// import IconCss3 from 'react-devicon/css3/original-wordmark';
-// import IconHtml5 from 'react-devicon/html5/original-wordmark';
-// import IconPostgresql from 'react-devicon/postgresql/original-wordmark';
-// import IconGit from 'react-devicon/git/original-wordmark';
+import Icon from '@iconify/react';
+import redux from '@iconify/icons-logos/redux';
+import javascript from '@iconify/icons-logos/javascript';
+import ruby from '@iconify/icons-logos/ruby';
+import rails from '@iconify/icons-logos/rails';
+import react from '@iconify/icons-logos/react';
+import github from '@iconify/icons-logos/github-icon';
+import mysql from '@iconify/icons-logos/mysql';
+import css from '@iconify/icons-logos/css-3';
+import html from '@iconify/icons-logos/html-5';
+import postgresql from '@iconify/icons-logos/postgresql';
+import heroku from '@iconify/icons-logos/heroku';
 
 class About extends Component {
     render() {
@@ -15,22 +18,23 @@ class About extends Component {
             <div className="about-section" id="about">
                 <div className="about-description">
                     <p>I am a Software Engineer with a deep desire to leverage my fluency in Russian and Ukrainian to make a deep impact in technology. </p> 
-                    <p>It has always been my dream to become some kind of engineer. While writing career goals in high school, writing down that I wanted to become an engineer always put a smile on my face. It kept me motivated and my desire to become an engineer grew everyday. </p>
                     <p>In college, I planned to become an Electrical Engineer. After taking some electrical circuit classes, my desire for that path slowly died. I decided to pivot and started taking classes in computer science. Building out small GUI (graphic-user-interface) applications with Visual Basic proved to be much more engaging for me. Everything clicked together and I knew I had a passion for coding. I loved looking at a screen with multiple lines of multi-color words. I loved writing code and watching my code come to life.</p>
                     <p>In present day, I have strong technical skills in Ruby on Rails, JavaScript, React, Redux, HTML, and CSS. I also have experience using MySQL, PostgreSQL, Heroku, various styling frameworks </p>
                     <p>Currently, I am looking for new contract or full time, front-end or full stack software engineering and developer opportunities in the Greater Seattle Area.</p>
                 </div>
-                {/* <div className="LanguageIcons">
-                    <IconRuby width={100} height={100}/>
-                    <IconRails width={100} height={100}/>
-                    <IconNodejs width={100} height={100}/>
-                    <IconJavascript width={100} height={100}/>
-                    <IconReact width={100} height={100}/>
-                    <IconCss3 width={100} height={100}/>
-                    <IconHtml5 width={100} height={100}/>
-                    <IconPostgresql width={100} height={100}/>
-                    <IconGit width={100} height={100}/> 
-                </div> */}
+                <div className="icons">
+                    <Icon icon={javascript} width={75} height={75} className="icon"/>
+                    <Icon icon={react} width={75} height={75} className="icon"/>
+                    <Icon icon={redux} width={75} height={75} className="icon"/>
+                    <Icon icon={ruby} width={75} height={75} className="icon"/>
+                    <Icon icon={rails} width={75} height={75} className="icon"/>
+                    <Icon icon={github} width={75} height={75} className="icon"/>
+                    <Icon icon={mysql} width={75} height={75} className="icon"/>
+                    <Icon icon={postgresql} width={75} height={75} className="icon"/>
+                    <Icon icon={css} width={75} height={75} className="icon"/>
+                    <Icon icon={html} width={75} height={75} className="icon"/>
+                    <Icon icon={heroku} width={75} height={75} className="icon"/>
+                </div>
             </div>
         )
     }
