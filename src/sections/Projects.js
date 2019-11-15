c217657fff5babbe3c4683476b5f499660059617
VadimS4 <github email address>
2019-11-15 00:16:10 -0800

    more mobile enhancement as well as changes to navbar and project cards

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -8,7 +8,7 @@ class Projects extends Component {
         return(
             <div className="projects-section" id="projects">
                 <div className="projectCards">
-                    <Card.Group itemsPerRow={1}>
+                    <Card.Group >
                         <Card className="Card">
                             <Image src={require('../images/FishLand.png')} wrapped-ui={false} />
                             <Card.Content>
