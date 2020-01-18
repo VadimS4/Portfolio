67059ff6fd69dff3fc1f5469a9ed1c0b81053e9d
VadimS4 <github email address>
2020-01-17 17:47:53 -0800

    optimized for mobile. Starting mobile first approach

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -8,28 +8,26 @@ class Main extends Component {
     render() {
         return(
             <div className="main-section" id="main">
-                <div className="title">
+                <div className="main-content">
                     <h1 className="name">Vadim Stakhnyuk</h1>
-                </div>
-                <div className="subtitle">
-                    <p>Software Engineer</p>
-                </div>
-                <div className="icons">
-                    <Button href="https://github.com/VadimS4" target="_blank" className="githubButton">
-                        <Icon link size="huge" name="github" />
-                    </Button>
-                    <Button href="https://www.linkedin.com/in/vadimstakhnyuk/" target="_blank" className="linkedinButton">
-                        <Icon link size="huge" name="linkedin" />
-                    </Button>
-                    <Button href="https://twitter.com/VadimStakh" target="_blank" className="twitterButton">
-                        <Icon link size="huge" name="twitter" />
-                    </Button>
-                    <Button href="https://medium.com/@vstakhnyuk" target="_blank" className="mediumButton">
-                        <Icon link size="huge" name="medium" />
-                    </Button>
-                </div>
-                <div className="resume">
-                    <p>Download <a download="VadimStakhnyuk_Resume.pdf" href={require("../VadimStakhnyuk_Resume.pdf")} target="_blank" rel="noopener noreferrer"> Resume</a></p>
+                    <h2 className="title">Software Engineer</h2>
+                    <div className="icons">
+                        <Button href="https://github.com/VadimS4" target="_blank" className="githubButton">
+                            <Icon link size="huge" name="github" />
+                        </Button>
+                        <Button href="https://www.linkedin.com/in/vadimstakhnyuk/" target="_blank" className="linkedinButton">
+                            <Icon link size="huge" name="linkedin" />
+                        </Button>
+                        <Button href="https://twitter.com/VadimStakh" target="_blank" className="twitterButton">
+                            <Icon link size="huge" name="twitter" />
+                        </Button>
+                        <Button href="https://medium.com/@vstakhnyuk" target="_blank" className="mediumButton">
+                            <Icon link size="huge" name="medium" />
+                        </Button>
+                    </div>
+                    <div className="resume">
+                        <p>Download <a download="VadimStakhnyuk_Resume.pdf" href={require("../VadimStakhnyuk_Resume.pdf")} target="_blank" rel="noopener noreferrer"> Resume</a></p>
+                    </div>
                 </div>
                 {/* <div className="downArrow">
                     <Link
