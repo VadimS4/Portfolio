62db4a60a28370e2ccd235716156017e6c3656cc
VadimS4 <github email address>
2020-02-14 16:25:44 -0800

    added gh-pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/components/icon.min.css';
-import './index.css';
-import App from './App';
+import '../src/index.css';
+import App from '../src/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
