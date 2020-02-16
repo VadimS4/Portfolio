228036bd8d4aa3ba35ca0cc3d07b5a9b7c5917ff
VadimS4 <github email address>
2020-02-15 17:59:31 -0800

    fixing mobile issues after hosting website

---
+++ build/precache-manifest.711c1e0ea7fc9bca849935d6a5f4e1d9.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf2cadaa0be9ecf0b569c6bcd1a240c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2e644eecf6d4e9278644",
+    "url": "/Portfolio/static/css/2.85e454a3.chunk.css"
+  },
+  {
+    "revision": "cc71fad6f3fc03226a41",
+    "url": "/Portfolio/static/css/main.dae7716d.chunk.css"
+  },
+  {
+    "revision": "2e644eecf6d4e9278644",
+    "url": "/Portfolio/static/js/2.6eb0c2ae.chunk.js"
+  },
+  {
+    "revision": "c10776da89ef1c2550c38243b6ec0ea7",
+    "url": "/Portfolio/static/js/2.6eb0c2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc71fad6f3fc03226a41",
+    "url": "/Portfolio/static/js/main.cca95bf9.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "e0eac68c5fbaeee6f257c0fc2a39f690",
+    "url": "/Portfolio/static/media/CarFinder.e0eac68c.png"
+  },
+  {
+    "revision": "2c1c4ae4efc013f4bc59b17009e08763",
+    "url": "/Portfolio/static/media/FishLand.2c1c4ae4.png"
+  },
+  {
+    "revision": "82029a67a38be56ac45e389e597f9d9b",
+    "url": "/Portfolio/static/media/NewYorkTimes.82029a67.png"
+  },
+  {
+    "revision": "1fedd471745ce012a9e485b63c8c9c4b",
+    "url": "/Portfolio/static/media/NoteMinder.1fedd471.png"
+  },
+  {
+    "revision": "de4119a9a7e5a19495b6f47c5720fbdf",
+    "url": "/Portfolio/static/media/Software-Engineer-image.de4119a9.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "266c44f4d5e4b96af11fed54448b3ed1",
+    "url": "/Portfolio/static/media/main-background.266c44f4.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f0e9f9092f4b6fb248acd5045fe7c650",
+    "url": "/Portfolio/static/media/seattle_background.f0e9f909.jpg"
+  }
+]);
